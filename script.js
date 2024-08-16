83ddbba3f07ca037c1cfb8bcff2cfd98036418ee
shpokas270 <s.shpokas@gmail.com>
2024-08-16 10:20:43 +0300

    Finished computer logic for generating random rock, papper or scissors.

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 //RockPapperScissors script
 
-//Logic for getting a computer choice
+//Logic for getComputerChoice
+//1) First, need to create random function that displays Rock,Paper,Scissor strings.
+
+const getComputerChoice = function () {
+  const words = [`Rock`, `Paper`, `Scissors`];
+  const computerChoice = words[Math.trunc(Math.random() * words.length)];
+  console.log(`The computer choice: ${computerChoice}`);
+};
+
+const computer = getComputerChoice();
