8698c7ac1bc898e3df3a92dc2d82d1fd001e4028
shpokas270 <s.shpokas@gmail.com>
2024-08-16 11:53:54 +0300

    Tweaked player choice and computer functions implemented global choice possibilitys

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
 //RockPapperScissors scripts Global scope
 
-let humanScore = 0;
-let computerScore = 0;
+const choices = [`rock`, `paper`, "scissor"]; //Possible choices
 
-//Logic for getComputerChoice
-//1) First, need to create random function that displays Rock,Paper,Scissor strings.
+//1) Computer choice:
 
-const getComputerChoice = function () {
-  const words = [`Rock`, `Paper`, `Scissors`];
-  const computerChoice = words[Math.trunc(Math.random() * words.length)];
-  console.log(`The computer choice: ${computerChoice}`);
+const compChoice = function () {
+  const computerChoice = choices[Math.trunc(Math.random() * choices.length)];
+  return console.log(`Computer choice: ${computerChoice}`);
 };
-
-const computer = getComputerChoice();
-
 // 2) Human logic for choosing getHumanChoice.
 
-const getHumanChoice = function () {
-  let player = prompt(`Choose: rock, paper, scissors`);
-  console.log(`Players choice: ${player}`);
+const playerChoice = function () {
+  let playerInput = prompt(`Choose: rock, paper, scissors`).toLowerCase();
+  return console.log(`Player choice: ${playerInput}`);
 };
-getHumanChoice();
+
+// 3) Logic for single round
